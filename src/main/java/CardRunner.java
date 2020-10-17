ff6f49143ee9050a05108d1f009df93b9c693ba9
alyse lartigue <alyse1703@gmail.com>
2020-10-17 00:06:55 +0000

    my final code

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,21 +1,21 @@
 public class CardRunner {
     public static void main(String args[]){
-        // ☕
         TerrapinCard cardBilly = new TerrapinCard(20);
         TerrapinCard cardBrian = new TerrapinCard(30);
         cardBilly.payGourmet();
         cardBrian.payEconomical();
-        System.out.println("Billy: " + cardBilly);
-        System.out.println("Brian: " + cardBrian);
+        System.out.println("Billy: The card has " + cardBilly + " dollars");
+        System.out.println("Brian: The card has " + cardBrian + " dollars");
         cardBilly.loadMoney(20);
         cardBrian.payGourmet();
-        System.out.println("Billy: " + cardBilly);
-        System.out.println("Brian: " + cardBrian);
+        System.out.println("Billy: The card has " + cardBilly + " dollars");
+        System.out.println("Brian: The card has " + cardBrian + " dollars");
         cardBilly.payEconomical();
         cardBilly.payEconomical();
         cardBrian.loadMoney(50);
-        System.out.println("Billy: " + cardBilly);
-        System.out.println("Brian: " + cardBrian);
+         System.out.println("Billy: The card has " + cardBilly + " dollars");
+        System.out.println("Brian: The card has " + cardBrian + " dollars"); 
+
 
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,36 +1,45 @@
 public class TerrapinCard {
 private double balance;
 
-public TerrapinCard(double balanceAtStart)
-{
-balance = balanceAtStart;
+
+public TerrapinCard(double balanceAtStart){
+  balance = balanceAtStart;
+}
+public String toString(){
+    return "The card has " + balance + " dollars";
+  }
+
+public void payEconomical(){
+  if(balance >= 2.50){
+  balance -= 2.50;
+  }
+
 }
 
- public String toString(){
-      return "The card has " + balance + " dollars";
+
+public void payGourmet(){
+  if(balance >= 4.00){
+  balance -= 4.00;
+  }
+}
+
+
+  public void loadMoney(double amount){
+
+    if(amount < 0){
+    balance = balance;
+
+    }else if( (balance + amount) > 150){
+      balance = 150;
+    }else{
+      balance += amount;
     }
-    public void payEconomical(){
-      if(balance > 2.5){
-        balance -= 2.5;
-      }
-    }
-    public void payGourmet(){
-      if(balance > 4.0){
-        balance -= 4.0;
-      }
-    }
-    public void loadMoney(double amount){
-      if(amount < 0.0){
-        amount  = 0.0;
-        balance += amount;
-      }
-      if(amount > 150){
-        balance += 150;
-      } else {
-        balance += amount;
-      }
-    }
-    public double getBalance(){
-      return balance;
-    }
+
+  }
+
+  public double getBalance(){
+    return balance; 
+  }
+
+
 }
